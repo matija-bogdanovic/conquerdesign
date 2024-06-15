40abf48c965b820621f9a3fccae39f7a170bd129
matija-bogdanovic <matijabog45@gmail.com>
2024-06-15 18:12:09 +0200

    z index changes for work embodyment section

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
     <Hero />
     <Video />
     <ShowcaseCards />
-    <Work class="z-[10000] relative"/>
-    <WorkEmbodyment />
+    <Work class="z-[10000] relative" />
+    <WorkEmbodyment class="z-[10000] relative" />
   </section>
   <Quote />
   <section>
